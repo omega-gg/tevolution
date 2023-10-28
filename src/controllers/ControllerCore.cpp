76b71e42bcbdc3504e5ead2378ca2bdf7ddaf04b
bunjee <bunjee@omega.gg>
2023-10-28 10:46:16 +0200

    Core: Update version comment

--- src/controllers/ControllerCore.cpp
+++ src/controllers/ControllerCore.cpp
@@ -62,7 +62,7 @@ W_INIT_CONTROLLER(ControllerCore)
 //-------------------------------------------------------------------------------------------------
 // Static variables
 
-// NOTE: Also check version_windows, version_code and CFBundleVersion.
+// NOTE: Also check DataLocal_patch, version_windows, version_code and CFBundleVersion.
 static const QString CORE_VERSION = "1.0.0-3";
 
 static const int CORE_CACHE = 1048576 * 100; // 100 megabytes
